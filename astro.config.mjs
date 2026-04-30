5028219cae6a0d6940a31385d42b23c877ec5dac
Procalzado Admin <dm1034398960@gmail.com>
2026-04-29 20:32:43 -0500

    fix admin y producto

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
+import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
   site: 'https://procalzado.com',
+  output: 'server', // <--- AGREGA ESTA LÍNEA
   integrations: [sitemap()],
+  adapter: cloudflare(),
 });
