485802f81b47a43d3be85acf199cc6867fa680b0
Procalzado Admin <dm1034398960@gmail.com>
2026-05-13 09:14:25 -0500

    sitemap dinamico y robots para SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
-import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
   site: 'https://procalzado.com',
-  output: 'server', // <--- AGREGA ESTA LÍNEA
-  integrations: [sitemap()],
-  adapter: cloudflare(),
 });
